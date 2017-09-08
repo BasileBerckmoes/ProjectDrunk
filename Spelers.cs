409896252abddb3cfd2c6ecd20e590ebac78d641
basile berckmoes <berckmoes.basile@me.com>
2017-09-09 00:27:37 +0200

    Initial commit

---
+++ Spelers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectDrunk
+{
+    public class Spelers
+    {
+        String naam;
+        //String geslacht;
+        public Spelers(String naam)
+        {
+            this.naam = naam;
+        }
+    }
+}
